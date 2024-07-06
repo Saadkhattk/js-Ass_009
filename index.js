6bff84ed9266a95ffa9b75cc333e43f354476ae4
Saadkhattk <saadahmadkhattak512@gmail.com>
2024-07-06 13:18:43 +0500

    updated

---
+++ index.js
@@ -0,0 +1,17 @@
+// Task no 1: Table print krna hain
+// user se koi number le and uska table 10 tk print kare
+
+var table = prompt("Enter a number");
+
+for (var i = 1; i <= 10; i++) {
+  console.log(table + "  * " + i + " = " + (i*table) );
+} 
+
+// Task no 2: 
+// 2. ARRAY ELEMENTS KA SUM PRINT KARE
+// DESCRIPTION:
+// 1. user se 5 numbers le and unko aik aik kar k array me rakhe
+// 2. phr array elements ka sum find kar k print kare
+
+
+
